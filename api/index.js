9ef531eec533b95194928ded26eb7f4314290380
Julian Bottiglione <julian.bottiglione@epitech.eu>
2024-05-03 15:08:27 +0200

    [~] reworked code

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
-
+ 
 module.exports = async (req, res) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-	if (req.body.login === process.env.login) {
-		res.status(200).json({ message:"succ" }) );
-	} else {
-		res.status(400).json({ message:"failure" }) );
-	}
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.body.login === process.env.login && req.body.password === process.env.password) {
+        res.status(200).json({ message: 'success' });
+    } else {    
+        res.status(401).json({ message: 'Unauthorized' });  
+    }
 }
