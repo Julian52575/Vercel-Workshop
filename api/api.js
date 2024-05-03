ea404268aaefaf06e7e44410d6b7c8c36db5de86
Julian Bottiglione <julian.bottiglione@epitech.eu>
2024-05-03 14:53:21 +0200

    [fix] syntax 2

--- api/api.js
+++ api/api.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-	if (req.body.login == process.env(login)) {
+	if (req.body.login == process.env.login) {
 		res.status(200.json({ message:"succ" }) );
 	} else {
 		res.status(400.json({ message:"failure" }) );
